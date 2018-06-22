3838bd87566c28932b0496088c48bc8ef9ccd151
Charles Bermudez <awtsupey19@gmail.com>
2018-06-22 10:11:46 +0800

    remove

--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-unset($_SESSION["user_email"]);
-
-header("location: ../admin");
-
-?>
--- admin/viewcat/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//for MySQLi OOP
-	$conn = new mysqli('localhost', 'root', '', 'scommerce');
-	if($conn->connect_error){
-	   die("Connection failed: " . $conn->connect_error);
-	}
-
-?>
